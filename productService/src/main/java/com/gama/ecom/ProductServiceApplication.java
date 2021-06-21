60919ab21404a8a477cfae73bc18cbd81afc3674
Renjithlal <renjithlalp@gmail.com>
2021-06-21 21:41:25 +0530

    Add the swagger ui

--- productService/src/main/java/com/gama/ecom/ProductServiceApplication.java
+++ productService/src/main/java/com/gama/ecom/ProductServiceApplication.java
@@ -2,7 +2,13 @@ package com.gama.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 //@EnableJpaRepositories("com.gama.ecom.repo")
@@ -11,5 +17,14 @@ public class ProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 
 }
