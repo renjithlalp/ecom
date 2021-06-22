d73bdf11e858fcd3ac4c862b3147e36bf1a24a0b
Renjithlal <renjithlalp@gmail.com>
2021-06-22 18:28:36 +0530

    Add the gateway pattern iplementation to the module

---
+++ orderService/src/main/java/com/gama/ecom/controller/OrderController.java
@@ -0,0 +1,8 @@
+package com.gama.ecom.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+}
--- productService/src/main/java/com/gama/ecom/ProductServiceApplication.java
+++ productService/src/main/java/com/gama/ecom/ProductServiceApplication.java
@@ -3,7 +3,6 @@ package com.gama.ecom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
